5b39451d1dd692c25f7bdcdb80e3f73400d534e0
Marcel Martin <marcel.martin@scilifelab.se>
2016-09-09 14:57:25 +0200

    A setup.py file

---
+++ setup.py
@@ -0,0 +1,29 @@
+import sys
+from setuptools import setup
+
+if sys.version_info < (2, 6):
+	sys.stdout.write("At least Python 2.6 is required.\n")
+	sys.exit(1)
+
+
+setup(
+	name = 'xopen',
+	version = '0.1',
+	author = 'Marcel Martin',
+	author_email = 'mail@marcelm.net',
+	url = 'https://github.com/marcelm/xopen/',
+	description = 'Open compressed files transparently',
+	license = 'MIT',
+	py_modules = ['xopen'],
+	classifiers = [
+		"Development Status :: 5 - Production/Stable",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Cython",
+		"Programming Language :: Python :: 2.6",
+		"Programming Language :: Python :: 2.7",
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.3",
+		"Programming Language :: Python :: 3.4",
+		"Programming Language :: Python :: 3.5",
+	]
+)
