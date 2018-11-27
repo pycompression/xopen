897a3e09f52897f6141d63f65a0a6bc051d09b11
Marcel Martin <marcel.martin@scilifelab.se>
2018-11-27 23:34:37 +0100

    Add python_requires

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
 	extras_require={
 		'dev': ['pytest'],
 	},
+	python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
 	classifiers=[
 		"Development Status :: 4 - Beta",
 		"License :: OSI Approved :: MIT License",
