c9afc7f17c6bf143bc96989c4553d788e998e939
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2021-03-10 11:21:25 +0100

    Update isal version to 0.6.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     package_data={"xopen": ["py.typed"]},
     extras_require={
         'dev': ['pytest'],
-        ':python_implementation != "PyPy"': ['isal>=0.6.0']
+        ':python_implementation != "PyPy"': ['isal>=0.6.1']
     },
     python_requires='>=3.6',
     classifiers=[
