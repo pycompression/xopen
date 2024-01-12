16794dd244ececc8110e74aaf06b5f75f611ac57
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2024-01-12 14:01:34 +0100

    Choose level 1 as default

--- src/xopen/__init__.py
+++ src/xopen/__init__.py
@@ -54,8 +54,8 @@ from ._version import version as __version__
 # 128K buffer size also used by cat, pigz etc. It is faster than the 8K default.
 BUFFER_SIZE = max(io.DEFAULT_BUFFER_SIZE, 128 * 1024)
 
-# Compression level 6 is the default for most gzip applications
-XOPEN_DEFAULT_GZIP_COMPRESSION = 6
+# 1 is by far the fastest and most efficient level.
+XOPEN_DEFAULT_GZIP_COMPRESSION = 1
 
 igzip: Optional[ModuleType]
 isal_zlib: Optional[ModuleType]
